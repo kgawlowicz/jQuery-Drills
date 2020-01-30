0e1b2007e32a28a0a497f97b17a796a7228f3f96
kgawlowicz <kgawlowicz@gmail.com>
2020-01-30 09:49:32 -0600

    final

--- app.js
+++ app.js
@@ -6,28 +6,46 @@ $('#btnSubmit').click(function () {
     alert("button clicked");
 })
 
+
+$('#myForm').submit(function(){
+    alert($("#inputText").val());
+})
+
+
 let $div = $('<div></div>');
 $('body').append($div);
+
+let $ul = $('<ul></ul>');
+$('body').append($ul);
+
 
 //Step 6
 $('#myForm').submit(function (e) {
 
     //prevent page from reloading and taking h2 away
     e.preventDefault();
-    let $h2 = $('<h2></h2>')
-    $h2.text($('#inputText').val())
+    // let $h2 = $('<h2></h2>')
+    // $h2.text($('#inputText').val())
+    // $div.append($h2)
+    let $li = $('<li></li>')
+    $li.text($('#inputText').val())
+    $ul.append($li)
+    $li.click(function(){
+        $li.css('color', getRandomColor())  
+    })
+    $li.dblclick(function(){
+        $li.remove();
+    })
+    
+    //alert($('#inputText').val());
 
-    $div.append($h2)
-
-    //alert($('#myForm').val(innertext));
-    //alert(this.elements[""0""].value);
-    //alert(this.value);
-    alert($('#inputText').val());
-
-    $h2.hover(
-        function () { $h2.css('background-color', 'lightBlue') },
-        function () { $h2.css('border-radius', '10px') }
-    )
+    // $h2.hover(function () { 
+    //         $h2.css({
+    //             'background-color': 'lightBlue',
+    //             'border-radius': '10px'
+    //         });
+    //     }
+    // )
 })
 
 
@@ -41,10 +59,37 @@ $('#inputText').keyup(function (e) {
 
 let $myForm = $('#myForm');
 
-//Step 8 - I have no idea if this is right
+// $('#btnSubmit2').click(function (e) {
+//     e.preventDefault();
+//     var submitValue = $('#textInput').val();
+//     //alert(submitValue);
+//     let item = $('<ul>' + submitValue + '<ul>');
+//     item.mouseover(function () {
+//         $(item).css('background-color', 'blue', );
+//         $(item).css('border-radius', '20px');
 
-//Step 9 
+//     });
 
+
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#'
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+
+    item.click(function () {
+        $(item).css('color', getRandomColor())
+
+    })
+    item.dblclick(function(){
+        item.remove();
+    })
+    $('body').append(item)
+;
 
 
 
